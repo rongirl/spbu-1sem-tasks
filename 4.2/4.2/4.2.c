132a18d8115eed36fe6df9c03a27473d81502ef7
rongirl <gro.pretty.arabella@mail.ru>
2021-10-11 21:59:48 +0300

    Реализовала ввод из файла, реализовала findFrequentElement

--- 4.2/4.2/4.2.c
+++ 4.2/4.2/4.2.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
-#include "qsort.h"
 #include <stdbool.h>
 #include <locale.h>
+#include <stdlib.h>
+#include "qsort.h"
+#include "search.h"
 
 int main()
-{   
-    
+{
     setlocale(LC_ALL, "rus");
-    
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL)
+    {
+        printf("file not found");
+        return 1;
+    }
+    int size = 0;
+    int array[100] = { 0 };
+    while (!feof(file))
+    {
+        fscanf(file, "%d", &array[size]);
+        ++size;
+    }
+    fclose(file);
+    if (size > 0)
+    {
+        printf("Ёлементы массива: ");
+        for (int i = 0; i < size; i++)
+        {
+            printf("%d ", array[i]);
+        }
+    }
+    printf("\n");
+    const int answer = findFrequentElement(array, size);
+    printf("—амый частый элемент: %d", answer);
 }
