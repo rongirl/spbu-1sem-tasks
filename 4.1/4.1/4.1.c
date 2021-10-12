1f1e78fdb370ed04efa561ff76346ea2f590e065
rongirl <gro.pretty.arabella@mail.ru>
2021-10-12 09:31:43 +0300

    Реализовала convertToDecimal

--- 4.1/4.1/4.1.c
+++ 4.1/4.1/4.1.c
@@ -11,9 +11,25 @@ void convertToBinary(int* binaryNumber, int number, int size)
 	}
 }
 
+int convertToDecimal(int* binaryNumber, int number, int size)
+{
+	int powerOfTwo = 1;
+	int decimalNumber = 0;
+	for (int i = size - 1; i >= 0; i--)
+	{
+		decimalNumber += powerOfTwo * binaryNumber[i];
+		powerOfTwo *= 2;
+	}
+	return decimalNumber;
+}
+
 int main()
 {   
     int size = 32;
+	int numberOne = 0;
+	int numberTwo = 0;
+	printf("¬ведите два числа: ");
+	scanf("%d %d", &numberOne, &numberTwo);
 	int* binaryNumber = calloc(size, sizeof(int));
 	
 	free(binaryNumber);
