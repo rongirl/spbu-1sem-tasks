be9ae78c109c76ad6d8622e1eb726e5cbfc17577
rongirl <gro.pretty.arabella@mail.ru>
2021-10-18 10:21:15 +0300

    Реализовала countOfNotesInPhoneBook()

--- homework4/4.3/4.3/4.3.c
+++ homework4/4.3/4.3/4.3.c
@@ -5,37 +5,97 @@
 
 typedef struct
 {
-	char name[50];
-	char phone[50];
+    char name[50];
+    char phone[50];
 }Entry;
 
 void begin()
 {
-	printf("Введите: ");
-	printf("0 - выйти \n");
-	printf("1 - добавить запись (имя и телефон) \n");
-	printf("2 - распечатать все имеющиеся записи \n");
-	printf("3 - найти телефон по имени \n");
-	printf("4 - найти имя по телефону \n");
-	printf("5 - сохранить текущие данные в файл \n");
+    printf("Введите: ");
+    printf("0 - выйти \n");
+    printf("1 - добавить запись (имя и телефон) \n");
+    printf("2 - распечатать все имеющиеся записи \n");
+    printf("3 - найти телефон по имени \n");
+    printf("4 - найти имя по телефону \n");
+    printf("5 - сохранить текущие данные в файл \n");
+}
+
+int countOfNotesInPhoneBook(Entry* PhoneBook, char const filename[])
+{
+    FILE* file = fopen(filename, "r");
+    int countOfNotes = 0;
+	if (file == NULL)
+    {
+			printf("File not found");
+			return -1;
+		}
+		char* data[200] = { 0 };
+		int linesRead = 0;
+		while (!feof(file))
+		{
+			char* buffer = malloc(sizeof(char) * 100);
+			if (buffer == NULL)
+			{
+				printf(":(");
+				fclose(file);
+				return -1;
+			}
+			const char* readBytes = fgets(buffer, 100, file);
+			if (readBytes < 0)
+			{
+				free(buffer);
+				break;
+			}
+			data[linesRead] = buffer;
+			++linesRead;
+		}
+		fclose(file);
+		for (int i = 0; i < linesRead - 1; i += 2)
+		{
+			strcpy(PhoneBook[countOfNotes].name, data[i]);
+			strcpy(PhoneBook[countOfNotes].phone, data[i + 1]);
+			++countOfNotes;
+		}
+		for (int i= 0; i < linesRead; i++)
+		{
+			free(data[i]);
+		}
+		return countOfNotes;
+}
+
+void saveData(Entry* PhoneBook, const char filename[], int countOfNotes)
+{
+    FILE* file = fopen(filename, "w");
+    if (file == NULL)
+    {
+        printf("File not found");
+        return -1;
+    }
+    for (int i = 0; i < countOfNotes; i++)
+    {
+        fprintf(file, "%s ", PhoneBook[i].name);
+        fprintf(file, "%s\n", PhoneBook[i].phone);
+    }
+    fclose(file);
 }
 
 void addNote(Entry* PhoneBook, const char name[], const char phone[], int countOfNotes)
 {
-	strcpy(PhoneBook[countOfNotes].name, name);
-	strcpy(PhoneBook[countOfNotes].phone, phone);
+    strcpy(PhoneBook[countOfNotes].name, name);
+    strcpy(PhoneBook[countOfNotes].phone, phone);
 }
 
 void printAllNotes(Entry* PhoneBook, int countOfNotes)
 {
-	if (countOfNotes == 0)
-	{
+    if (countOfNotes == 0)
+    {
 		printf("Нет записей\n");
 		return;
 	}
 	for (int i = 0; i < countOfNotes; i++)
 	{
-		printf("%s - %s\n", PhoneBook[i].name, PhoneBook[i].phone);
+		printf("%s", PhoneBook[i].name);
+		printf("%s\n", PhoneBook[i].phone);
 	}
 }
 
@@ -83,7 +143,9 @@ int main()
 	++countOfNotes;
 	addNote(PhoneBook, name, phone, countOfNotes);
 	++countOfNotes;
-	printAllNotes(PhoneBook, countOfNotes);
-	printf("%s", findName(PhoneBook, "89595955", countOfNotes));
+    saveData(PhoneBook, "base.txt", countOfNotes);
+	//printAllNotes(PhoneBook, countOfNotes);
+	//printf("%s", findName(PhoneBook, "89595955", countOfNotes));
 	//printf("%s %s", PhoneBook[0].name, PhoneBook[0].phone);
+
 }
