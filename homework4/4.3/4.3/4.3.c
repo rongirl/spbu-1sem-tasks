cc1466091a0004427259f857443f95ae496fcc6c
rongirl <gro.pretty.arabella@mail.ru>
2021-10-18 19:49:59 +0300

    Дополнила main

--- homework4/4.3/4.3/4.3.c
+++ homework4/4.3/4.3/4.3.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include <locale.h>
+#include <stdbool.h>
 
 typedef struct
 {
     char name[50];
     char phone[50];
-}Entry;
+} Entry;
 
 void begin()
 {
@@ -26,41 +27,41 @@ int countOfNotesInPhoneBook(Entry* PhoneBook, char const filename[])
     int countOfNotes = 0;
 	if (file == NULL)
     {
-			printf("File not found");
-			return -1;
-		}
-		char* data[200] = { 0 };
-		int linesRead = 0;
-		while (!feof(file))
-		{
-			char* buffer = malloc(sizeof(char) * 100);
-			if (buffer == NULL)
-			{
-				printf(":(");
-				fclose(file);
-				return -1;
-			}
-			const char* readBytes = fgets(buffer, 100, file);
-			if (readBytes < 0)
-			{
-				free(buffer);
-				break;
-			}
-			data[linesRead] = buffer;
-			++linesRead;
-		}
-		fclose(file);
-		for (int i = 0; i < linesRead - 1; i += 2)
-		{
-			strcpy(PhoneBook[countOfNotes].name, data[i]);
-			strcpy(PhoneBook[countOfNotes].phone, data[i + 1]);
-			++countOfNotes;
-		}
-		for (int i= 0; i < linesRead; i++)
-		{
-			free(data[i]);
-		}
-		return countOfNotes;
+        printf("File not found");
+        return -1;
+    }
+    char* data[200] = { 0 };
+    int linesRead = 0;
+    while (!feof(file))
+    {
+        char* buffer = malloc(sizeof(char) * 100);
+        if (buffer == NULL)
+        {
+            printf(":(");
+            fclose(file);
+            return -1;
+        }
+        const int readBytes = fscanf(file, "%s", buffer);
+        if (readBytes < 0)
+        {
+            free(buffer);
+            break;
+        }
+        data[linesRead] = buffer;
+        ++linesRead;
+    }
+    fclose(file);
+    for (int i = 0; i < linesRead - 1; i += 2)
+    {
+        strcpy(PhoneBook[countOfNotes].name, data[i]);
+        strcpy(PhoneBook[countOfNotes].phone, data[i + 1]);
+        ++countOfNotes;
+    }
+    for (int i = 0; i < linesRead; i++)
+    {
+        free(data[i]);
+    }
+    return countOfNotes;
 }
 
 void saveData(Entry* PhoneBook, const char filename[], int countOfNotes)
@@ -81,8 +82,13 @@ void saveData(Entry* PhoneBook, const char filename[], int countOfNotes)
 
 void addNote(Entry* PhoneBook, const char name[], const char phone[], int countOfNotes)
 {
-    strcpy(PhoneBook[countOfNotes].name, name);
-    strcpy(PhoneBook[countOfNotes].phone, phone);
+    if (countOfNotes < 100)
+    {
+        strcpy(PhoneBook[countOfNotes].name, name);
+        strcpy(PhoneBook[countOfNotes].phone, phone);
+        return;
+    }
+    printf("Лимит записей превышен.\n");
 }
 
 void printAllNotes(Entry* PhoneBook, int countOfNotes)
@@ -94,7 +100,7 @@ void printAllNotes(Entry* PhoneBook, int countOfNotes)
 	}
 	for (int i = 0; i < countOfNotes; i++)
 	{
-		printf("%s", PhoneBook[i].name);
+		printf("%s ", PhoneBook[i].name);
 		printf("%s\n", PhoneBook[i].phone);
 	}
 }
@@ -103,7 +109,7 @@ const char *findName(Entry* PhoneBook, const char phone[], int countOfNotes)
 {
 	if (countOfNotes == 0)
 	{
-		return "Нет записей\n";
+		return "Нет записей";
 	}
 	for (int i = 0; i < countOfNotes; i++)
 	{
@@ -112,14 +118,14 @@ const char *findName(Entry* PhoneBook, const char phone[], int countOfNotes)
 			return PhoneBook[i].name;
 		}
 	}
-	return "Не найдено\n";
+	return "Не найдено";
 }
 
-const char* findPhone(Entry* PhoneBook, const char name[], int countOfNotes)
+const char *findPhone(Entry* PhoneBook, const char name[], int countOfNotes)
 {
 	if (countOfNotes == 0)
 	{
-		return "Нет записей\n";
+		return "Нет записей";
 	}
 	for (int i = 0; i < countOfNotes; i++)
 	{
@@ -128,24 +134,71 @@ const char* findPhone(Entry* PhoneBook, const char name[], int countOfNotes)
 			return PhoneBook[i].phone;
 		}
 	}
-	return "Не найдено\n";
+	return "Не найдено";
 }
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	Entry PhoneBook[100] = { '\0' };
-	begin();
-	const char* name = "Ivan Ivanov";
-	const char* phone = "895959595";
-	int countOfNotes = 0;
-	addNote(PhoneBook, name, phone, countOfNotes);
-	++countOfNotes;
-	addNote(PhoneBook, name, phone, countOfNotes);
-	++countOfNotes;
-    saveData(PhoneBook, "base.txt", countOfNotes);
-	//printAllNotes(PhoneBook, countOfNotes);
-	//printf("%s", findName(PhoneBook, "89595955", countOfNotes));
-	//printf("%s %s", PhoneBook[0].name, PhoneBook[0].phone);
+    setlocale(LC_ALL, "rus");
+    Entry PhoneBook[100] = { '\0' };
+    begin();
+    bool flag = true;
+    int countOfNotes = countOfNotesInPhoneBook(PhoneBook, "base.txt");
+    while (flag)
+    {
+        char input = '\0';
+        scanf("%c", &input);
+        switch (input)
+        {
+            case '0':
+            {
+                flag = false;
+                break;
+            }
+            case '1':
+            {   
+                char name[50] = { '\0' };
+                printf("Введите имя:\n");
+                scanf("\n%[^\n]", name);
+                char phone[50] = { '\0' };
+                printf("Введите номер телефона:\n");
+                scanf("\n%[^\n]", phone);
+                addNote(PhoneBook, name, phone, countOfNotes);
+                ++countOfNotes;
+                break;
+            }
+            case '2':
+            {
+                printf("Все имеющиеся записи: \n");
+                printAllNotes(PhoneBook, countOfNotes);
+                break;
+            }
+            case '3':
+            {   
+                char name[50] = { '\0 ' };
+                printf("Введите имя:\n");
+                scanf("\n%[^\n]", name);
+                printf("%s\n", findPhone(PhoneBook, name, countOfNotes));
+                break;
+            }
+            case '4':
+            {
+                char phone[50] = { '\0 ' };
+                printf("Введите номер телефона:\n");
+                scanf("\n%[^\n]", phone);
+                printf("%s\n", findName(PhoneBook, phone, countOfNotes));
+                break;
+            } 
+            case '5':
+            {
+                saveData(PhoneBook, "base.txt", countOfNotes);
+                printf("Данные сохранены\n");
+            }
+        }
+
+
+    }
+
+
 
 }
