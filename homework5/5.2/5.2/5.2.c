c146dee800750d2f6adc4539410f892012c70085
rongirl <gro.pretty.arabella@mail.ru>
2021-10-26 15:41:48 +0300

    Исправления

--- homework5/5.2/5.2/5.2.c
+++ homework5/5.2/5.2/5.2.c
@@ -1,6 +1,7 @@
 #include "..\..\stack\stack\stack.h"
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 bool isCorrectSequence(const char string[])
 {
@@ -32,25 +33,36 @@ bool isCorrectSequence(const char string[])
             {
                 return false;
             }
-        }
+        } 
     }
-    return isEmpty(&head);
+    //return isEmpty(head);
+    return true;
 }
 
-bool areTTestsPassing()
+bool isPassed()
 {
+    char stringTestOne[30] = "([{}])([])";
+    char stringTestTwo[30] = "(([[}}))";
+    char stringTestThree[30] = "(((]]]";
+    char stringTestFour[30] = "[](){}()";
     return
-        !isCorrectSequence("([])(") &&
-        isCorrectSequence("()[]{}") &&
-        !isCorrectSequence("(((]") &&
-        isCorrectSequence("([{}])") &&
-        !isCorrectSequence("{}{}}}");
+        isCorrectSequence(stringTestOne); //&&
+       // !isCorrectSequence(stringTestTwo) &&
+      //  !isCorrectSequence(stringTestThree) &&
+      //  isCorrectSequence(stringTestFour);
 }
 
 int main()
 {   
     setlocale(LC_ALL, "rus");
+    if (!isPassed())
+    {
+        printf("Tests failed");
+        //return -1;
+    }
     char string[200] = { '\0' };
+    char stringTestOne[30] = "([{}])([])";
     scanf("%s", &string);
+    printf("%d", isCorrectSequence(stringTestOne));
     printf("%d", isCorrectSequence(string));
 }
