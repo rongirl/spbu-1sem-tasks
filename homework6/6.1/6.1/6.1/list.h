ee729c343e2cc1897c08d8f0f48657264821ead5
rongirl <gro.pretty.arabella@mail.ru>
2021-11-04 11:10:51 +0300

    Добавление комментариев

--- homework6/6.1/6.1/6.1/list.h
+++ homework6/6.1/6.1/6.1/list.h
@@ -14,22 +14,32 @@ typedef struct ListElement ListElement;
 // создание листа
 List* createList();
 
+// удаление листа, принимает список
 void deleteList(List* list);
 
+// удаление элемента по значению
 bool deleteValue(List* list, int value);
 
+// добавление значения в список, принимает список и значение
 void add(List* list, int value);
 
+// распечатывание списка, принимает список 
 void printList(List* list);
 
+// начальный вывод
 void begin();
 
+// возвращает позицию первого элемента
 Position* first(List* list);
 
+// возвращает позицию следующего элемента
 Position* next(Position* position);
 
+// проверка на последний элемент
 bool last(Position* position);
 
+// возвращает значение элемента по его позиции в списке 
 int get(List* list, Position* position);
 
+// проверка на пустоту списка
 bool isEmpty(List* list);
--- homework6/6.1/6.1/6.1/testList.h
+++ homework6/6.1/6.1/6.1/testList.h
@@ -2,6 +2,8 @@
 
 #include <stdbool.h>
 
+// проверка тестов
 bool areTestsPassed();
-
+ 
+// проверка на упорядоченность списка
 bool checkSort(List* list);
