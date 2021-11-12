ea135a4461596786c36468e4527723c54c905ab6
rongirl <gro.pretty.arabella@mail.ru>
2021-11-12 13:40:09 +0300

    Комментарии к функциям, исправления

--- homework5/stack/stack/stack.h
+++ homework5/stack/stack/stack.h
@@ -1,12 +1,18 @@
 #pragma once
+
 #include<stdbool.h>
 
+// структура элемента стека
 typedef struct StackElement StackElement;
 
+// добавление элемента на вершину стека
 void push(StackElement** head, int number);
 
+// удаление элемента с вершины стека
 int pop(StackElement** head);
 
+// проверка на пустоту стека
 bool isEmpty(StackElement* head);
 
+// удаление стека
 void deleteStack(StackElement** head);
--- homework5/stack/stack/testsOfStack.c
+++ homework5/stack/stack/testsOfStack.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "stack.h"
 #include <stdbool.h>
 
@@ -6,22 +5,26 @@ bool areTestsPassing()
 {
     StackElement* headTest = NULL;
     if (!isEmpty(headTest))
-    {
+    {   
+        deleteStack(&headTest);
         return false;
     }
     push(&headTest, 7);
     push(&headTest, 8);
     if (isEmpty(headTest))
     {
+        deleteStack(&headTest);
         return false;
     }
-    if (pop(&headTest) == 7)
-    {
+    if (pop(&headTest) != 8)
+    {   
+        deleteStack(&headTest);
         return false;
     }
     pop(&headTest);
     if (!isEmpty(headTest))
-    {
+    {   
+        deleteStack(&headTest);
         return false;
     }
     push(&headTest, 9);
