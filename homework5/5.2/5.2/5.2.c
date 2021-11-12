865c46ccacf3aaa27829fd50704aebbe782eb1f9
rongirl <gro.pretty.arabella@mail.ru>
2021-11-12 15:48:16 +0300

    Устранила утечки памяти, исправила тесты

--- homework5/5.2/5.2/5.2.c
+++ homework5/5.2/5.2/5.2.c
@@ -6,7 +6,8 @@
 bool isCorrectSequence(const char string[])
 {
     StackElement* head = NULL;
-    for (int i = 0; i < 200; i++)
+    int length = strlen(string);
+    for (int i = 0; i < length; i++)
     {   
         if (string[i] != '\0' && string[i] != '\n')
         {
@@ -17,42 +18,40 @@ bool isCorrectSequence(const char string[])
             else if (!isEmpty(head))
             {
                 if (string[i] == ')' && pop(&head) != '(')
-                {
+                {   
+                    deleteStack(&head);
                     return false;
                 }
                 else if (string[i] == '}' && pop(&head) != '{')
-                {
+                {   
+                    deleteStack(&head);
                     return false;
                 }
                 else if (string[i] == ']' && pop(&head) != '[')
-                {
+                {   
+                    deleteStack(&head);
                     return false;
                 }
             }
             else
-            {
+            {   
+                deleteStack(&head);
                 return false;
             }
         } 
     }
-    return isEmpty(head);
+    bool isStackEmpty = isEmpty(head);
+    deleteStack(&head);
+    return isStackEmpty;
 }
 
 bool isPassed()
 {
-    char stringTestOne[200] = { '\0' };
-    char stringTestTwo[200] = { '\0' } ;
-    char stringTestThree[200] = { '\0' };
-    char stringTestFour[200] = { '\0' };
-    strcpy(stringTestOne, "([{}])([])");
-    strcpy(stringTestTwo, "(([[}}))");
-    strcpy(stringTestThree, "(((]]]");
-    strcpy(stringTestFour, "[](){}()");
     return
-        isCorrectSequence(stringTestOne) &&
-        !isCorrectSequence(stringTestTwo) &&
-        !isCorrectSequence(stringTestThree) &&
-        isCorrectSequence(stringTestFour);
+        isCorrectSequence("([{}])([])") &&
+        !isCorrectSequence("(([[}}))") &&
+        !isCorrectSequence("(((]]]") &&
+        isCorrectSequence("[](){}()");
 }
 
 int main()
