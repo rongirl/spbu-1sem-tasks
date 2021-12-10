b29ff850564da59436876c2acee1f636d1eaeb21
rongirl <gro.pretty.arabella@mail.ru>
2021-12-10 20:14:58 +0300

    Добавление комментариев

--- homework8/8.1/8.1/8.1/avlTree.h
+++ homework8/8.1/8.1/8.1/avlTree.h
@@ -2,18 +2,26 @@
 
 #include <stdbool.h>
 
+// структура дерева
 typedef struct Dictionary Dictionary;
 
+// создание дерева 
 Dictionary* createTree();
 
+// проверка на пустоту дерева
 bool isEmpty(Dictionary* tree);
 
+// удаление дерева
 void deleteTree(Dictionary** tree);
 
+// взятие значения элемента по ключу 
 const char* getValue(Dictionary** tree, const char* key);
 
+// добавление элемента в дерево
 void addValue(Dictionary* tree, const char* key, const char* value);
 
+// удаление элемента по ключу 
 void deleteValue(Dictionary** tree, const char* key);
 
+// проверка на присутствие элемента в дереве по ключу
 bool inTree(Dictionary** tree, const char* key);
