38eb5de225971a2ef48c48c4eab92fc306879224
rongirl <gro.pretty.arabella@mail.ru>
2021-12-17 19:16:27 +0300

    Добавление комментариев

--- homework9/9.1/9.1/9.1/list.h
+++ homework9/9.1/9.1/9.1/list.h
@@ -2,20 +2,29 @@
 
 #include <stdbool.h>
 
+// структура списка
 typedef struct List List;
 
+// создание списка
 List* createList();
 
+// добавление элемента в список
 bool addInList(List* list, const char* word, int count);
 
+// удаление вершины списка
 void deleteHead(List* list);
 
+// удаление списка
 void deleteList(List* list);
 
+// проверка на пустоту списка
 bool isEmpty(List* list);
 
+// взятие значения элемента списка
 char const* getWordHead(List* list);
 
+// удаление вершины списка
 void deleteHead(List* list);
 
+// взятие количества элементов в вершине списка
 int getCountHead(List* list);
